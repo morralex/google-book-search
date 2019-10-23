2cb85e7ab7889296328b1d8ff8499ca8bbdfc199
morralex <morr.alex@outlook.com>
2019-10-22 17:55:19 -0700

    2nd try

---
+++ client/build/precache-manifest.d96640547fc842698df28d77f7423b81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77e72519fd23f1aa67c9",
+    "url": "/static/css/main.2552c430.chunk.css"
+  },
+  {
+    "revision": "77e72519fd23f1aa67c9",
+    "url": "/static/js/main.42082b8c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1b8b22b5e811e0ea8c65",
+    "url": "/static/js/2.3724a3bc.chunk.js"
+  },
+  {
+    "revision": "5263f434c09d9a9a1a50c5037ee59dcb",
+    "url": "/index.html"
+  }
+];
